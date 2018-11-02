b944d5f2e381cae90c4dd37010f715d2a047ed65
J Kim <janice.juyeon.kim@gmail.com>
2018-11-02 14:19:02 -0400

    Adding check for image links in book

--- src/Book.js
+++ src/Book.js
@@ -12,7 +12,14 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail || ""})` }}></div>
+          { (book.imageLinks !== undefined) ?
+              <div
+                className="book-cover"
+                style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail })` }}>
+              </div>
+              :
+              <div className="book-cover"></div>
+          }
           <ShelfSelector selected={book.shelf} />
         </div>
         <div className="book-title">{book.title}</div>
